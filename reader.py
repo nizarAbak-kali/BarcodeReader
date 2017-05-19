aeccc6278671a9e67d6ec5f0b17ee56e36ae5045
Nizar Abak-kali <nizar.abak-kali@smile.fr>
2017-05-19 22:50:09 +0200

    presque fini manque recup d'image

--- reader.py
+++ reader.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import zbar
+import zbar.misc
 
 
 def get_barcode():
     print "reading ...."
 
     scanner = zbar.ImageScanner()
+    image = read_image_into_numpy_array(...) # get an image into a numpy array
+    scanner = zbar.Scanner()
+    results = scanner.scan(image)
+
+    for result in results:
+        if result.type == 'UPC-A':
+            print(result.data, zbar.misc.upca_is_valid(result.data.decode('ascii')))
+
 
 
 if __name__ == '__main__':
-
     get_barcode()
