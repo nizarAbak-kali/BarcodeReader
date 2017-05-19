c8dc704ea9ee38c53a2fac690163189df460ae50
Nizar Abak-kali <nizar.abak-kali@smile.fr>
2017-05-19 22:44:48 +0200

    first commit

---
+++ reader.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import zbar
+
+
+def get_barcode():
+    print "reading ...."
+
+    scanner = zbar.ImageScanner()
+
+
+if __name__ == '__main__':
+
+    get_barcode()
